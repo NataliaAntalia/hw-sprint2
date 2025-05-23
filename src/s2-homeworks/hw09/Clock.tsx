2a343a479aee8b1bbfe0fe734d585a2bc1341682
Natalia <nataliaantaliaa@gmail.com>
2025-05-23 22:13:09 +0300

    uploaded the nine hw

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -10,77 +10,83 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const start = () => {
-        // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
-        // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
-    }
-
-    const stop = () => {
-        // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
-    }
-
-    const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
-    }
-    const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-
-    }
-
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
-
-    // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
-
-    return (
-        <div className={s.clock}>
-            <div
-                id={'hw9-watch'}
-                className={s.watch}
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                <span id={'hw9-day'}>{stringDay}</span>,{' '}
-                <span id={'hw9-time'}>
-                    <strong>{stringTime}</strong>
-                </span>
-            </div>
-
-            <div id={'hw9-more'}>
-                <div className={s.more}>
-                    {show ? (
-                        <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
-                        </>
-                    ) : (
-                        <>
-                            <br/>
-                        </>
-                    )}
-                </div>
-            </div>
-
-            <div className={s.buttonsContainer}>
-                <SuperButton
-                    id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
-                    onClick={start}
-                >
-                    start
-                </SuperButton>
-                <SuperButton
-                    id={'hw9-button-stop'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер не запущен
-                    onClick={stop}
-                >
-                    stop
-                </SuperButton>
-            </div>
-        </div>
-    )
+        if (timerId) {
+            clearInterval(timerId)
+        }
+        const id = window.setInterval(() => {
+            setDate(new Date()) // обновляем текущую дату каждую секунду
+        }, 1000)
+        setTimerId(id)
 }
 
-export default Clock
+        const stop = () => {
+            if (timerId) {
+                clearInterval(timerId)
+                setTimerId(undefined)
+            }
+        }
+
+        const onMouseEnter = () => {
+            setShow(true)
+        }
+        const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+            setShow(false)
+        }
+
+        const stringTime = date.toLocaleTimeString('ru-RU', { hour12: false }) || <br/>
+        const stringDate = date.toLocaleDateString('ru-RU') || <br/>
+
+        const stringDay = date.toLocaleDateString('en-US', { weekday: 'long' }) || <br/>
+        const stringMonth = date.toLocaleDateString('en-US', { month: 'long' }) || <br/>
+
+        return (
+            <div className={s.clock}>
+                <div
+                    id={'hw9-watch'}
+                    className={s.watch}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                >
+                    <span id={'hw9-day'}>{stringDay}</span>,{' '}
+                    <span id={'hw9-time'}>
+                    <strong>{stringTime}</strong>
+                </span>
+                </div>
+
+                <div id={'hw9-more'}>
+                    <div className={s.more}>
+                        {show ? (
+                            <>
+                                <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                                <span id={'hw9-date'}>{stringDate}</span>
+                            </>
+                        ) : (
+                            <>
+                                <br/>
+                            </>
+                        )}
+                    </div>
+                </div>
+
+                <div className={s.buttonsContainer}>
+                    <SuperButton
+                        id={'hw9-button-start'}
+                        disabled={!!timerId}
+                        onClick={start}
+                    >
+                        start
+                    </SuperButton>
+                    <SuperButton
+                        id={'hw9-button-stop'}
+                        disabled={!timerId}
+                        onClick={stop}
+                    >
+                        stop
+                    </SuperButton>
+                </div>
+            </div>
+        )
+
+}
+    export default Clock
