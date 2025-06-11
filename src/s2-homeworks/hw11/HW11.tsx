68d0bb256aa3caf8187ebdfd1cac053018a2a545
Natalia <nataliaantaliaa@gmail.com>
2025-06-11 16:02:36 +0300

    fixed hw 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -34,20 +34,21 @@ function HW11() {
                 <div className={s.container}>
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
+                        <div style={{ width: 1000 }}>
                         <SuperRange value={value1} onChange={change}
                             id={'hw11-single-slider'}
 
-                            // сделать так чтоб value1 изменялось // пишет студент
 
                         />
+                        </div>
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
+                        <div style={{ width: 1000 }}>
                         <SuperRange value={[value1, value2]} onChange={change}
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
+                            id={'hw11-double-slider'}/>
+                        </div>
 
-                        />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
                 </div>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,12 +4,10 @@ import s from '..//../HW11.module.css'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <div id={props.id}>
         <Slider className={s.Slider}
 
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
-        </div>
     )
 }
 
