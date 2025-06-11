0deb0c70a3b5c6efcf9ecb9fe9e291a6c8a3c6e2
Natalia <nataliaantaliaa@gmail.com>
2025-06-11 15:23:55 +0300

    fixed hw 11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,10 +4,12 @@ import s from '..//../HW11.module.css'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
+        <div id={props.id}>
         <Slider className={s.Slider}
 
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
+        </div>
     )
 }
 
